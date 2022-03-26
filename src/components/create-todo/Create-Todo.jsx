9b1a920ad35b90ce30a8ee4e5b4dc6a66fb2932c
uzak <uzak5581@gmail.com>
2022-03-26 20:16:01 +0600

    adding two components header/createTodo/styling

---
+++ src/components/create-todo/Create-Todo.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+const CreateTodoList =()=>{
+    return(
+        <div>gjjkgjj</div>
+    )
+}
+export default CreateTodoList
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css"
+
+const Header =()=>{
+    return(
+        <div className="HeaderWrapper">
+            hello
+        </div>
+
+    )
+}
+export default Header
